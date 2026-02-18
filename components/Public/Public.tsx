f2f1df18eaaa22d21951ceb95bf38c9d03e60170
MrPapperlapapp <krivmann00@gmail.com>
2026-02-18 11:24:13 +0300

    feat: add public route

--- components/Public/Public.tsx
+++ components/Public/Public.tsx
@@ -6,19 +6,19 @@ import { useRouter } from 'next/navigation';
 import { PropsWithChildren, useEffect } from 'react';
 import { Center, Loader } from '@mantine/core';
 
-import classes from './Protected.module.css';
+import classes from './Public.module.css';
 
-export default function Protected({ children }: PropsWithChildren) {
+export default function Public({ children }: PropsWithChildren) {
   const { user, isLoading } = useSelector((state: RootState) => state.auth);
   const router = useRouter();
 
   useEffect(() => {
-    if (!isLoading && !user) {
-      router.replace('/login');
+    if (!isLoading && user) {
+      router.replace('/api');
     }
   }, [isLoading, user, router]);
 
-  if (isLoading) {
+  if (isLoading || user) {
     return (
       <Center className={classes.loader}>
         <Loader size="lg" />
